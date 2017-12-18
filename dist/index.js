2e45b5d5f10fbd526e3e0df7abd6e99f5b57fdec
Joe Plahitko <joseph.plahitko@egov.com>
2017-12-17 19:04:15 -0500

    Expose Model and Rule

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,6 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Model_1 = require("./Model");
+exports.Model = Model_1.Model;
+var Rule_1 = require("./Rule");
+exports.Rule = Rule_1.Rule;
