f73bf39de2710983596d9253ead5afe607312fad
Joe Plahitko <joseph.plahitko@egov.com>
2017-12-22 14:29:08 -0500

    Abstract for TQualifierCollection

---
+++ dist/abstract/TQualifierCollection.d.ts
@@ -0,0 +1,3 @@
+import { TQualifier } from "./TQualifier";
+import { TQualifierMeta } from "./TQualifierMeta";
+export declare type TQualifierCollection = Map<TQualifier, TQualifierMeta>;
---
+++ src/abstract/TQualifierCollection.ts
@@ -0,0 +1,4 @@
+import { TQualifier } from "./TQualifier";
+import { TQualifierMeta } from "./TQualifierMeta";
+
+export type TQualifierCollection = Map<TQualifier, TQualifierMeta>;
