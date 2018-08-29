e2bcac9f44263508d140d1a1d634c781a9b8a89d
Joe Plahitko <joseph.plahitko@egov.com>
2018-08-29 01:44:47 -0400

    Fixed ICollectionFluentInterface methods returning incorrect type.

--- src/abstract/ICollectionFluentInterface.ts
+++ src/abstract/ICollectionFluentInterface.ts
@@ -3,13 +3,14 @@ import { IValidatable } from './IValidatable';
 import { CollectionRule } from '../concrete/CollectionRule';
 import { TPrecondition } from './TPrecondition';
 import { TQualifier } from './TQualifier';
+import { ISimpleFluentInterface } from './ISimpleFluentInterface';
 
 export interface ICollectionFluentInterface {
-	length(num1: number, num2: number): ICollectionFluentInterface;
-	lengthOrEmpty(num1: number, num2: number): ICollectionFluentInterface;
-	must(qualifier: TQualifier): ICollectionFluentInterface;
-	notEmpty(): ICollectionFluentInterface;
-	notNull(): ICollectionFluentInterface;
+	length(num1: number, num2: number): ISimpleFluentInterface;
+	lengthOrEmpty(num1: number, num2: number): ISimpleFluentInterface;
+	must(qualifier: TQualifier): ISimpleFluentInterface;
+	notEmpty(): ISimpleFluentInterface;
+	notNull(): ISimpleFluentInterface;
 	stopOnFirstFailure(): void;
 	cascade(): void;
 	using(validatable: IValidatable): ICollectionFluentInterface;
